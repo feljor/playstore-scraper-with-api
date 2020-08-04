7d0b483fd897be98a8f67ff56d17ac1407be238a
Feljor <sitefeljor@gmail.com>
2020-08-04 02:31:24 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const Express = require('express');
+const router = require('./lib');
+
+const app = Express();
+const port = process.env.PORT || 5000;
+
+app.use('/api/', router);
+
+app.listen(port, function () {
+  console.log('Server started on port', port);
+});
